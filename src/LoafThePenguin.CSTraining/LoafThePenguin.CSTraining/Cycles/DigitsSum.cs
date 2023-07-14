c7ea9405d274e659e875c9e810b4c51a73aa7aa5
Михаил Лоскутов <dev.toriki@gmail.com>
2023-07-14 17:41:25 +0500

    Добавление задачи вычисления суммы цифр числа.

---
+++ src/LoafThePenguin.CSTraining/LoafThePenguin.CSTraining/Cycles/DigitsSum.cs
@@ -0,0 +1,14 @@
+using LoafThePenguin.CSTraining.Core;
+
+namespace LoafThePenguin.CSTraining.Cycles;
+
+/*
+ * Написать алгоритм, который вычислит сумму всех цифр числа.
+ */
+public sealed class DigitsSum : IAlgorithm<int, int>
+{
+    public int Run(int input)
+    {
+        throw new NotImplementedException();
+    }
+}
