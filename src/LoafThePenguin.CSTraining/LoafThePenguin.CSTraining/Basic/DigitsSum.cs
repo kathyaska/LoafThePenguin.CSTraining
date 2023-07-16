3652d0e68812a80885c81ab8859b86aabfe1a80e
Екатерина <kathyaska@mail.ru>
2023-07-16 22:51:38 +0300

    написал алгоритм, который вычислил сумму всех цифр числа

--- src/LoafThePenguin.CSTraining/LoafThePenguin.CSTraining/Basic/DigitsSum.cs
+++ src/LoafThePenguin.CSTraining/LoafThePenguin.CSTraining/Basic/DigitsSum.cs
@@ -9,6 +9,18 @@ public sealed class DigitsSum : IAlgorithm<int, int>
 {
     public int Run(int input)
     {
-        throw new NotImplementedException();
+
+        int sum = 0; 
+        if (input < 0)
+        {
+            input *= -1; 
+        }
+        do
+        {
+            int num = input % 10;
+            input = (input - num) / 10;
+            sum += num;
+        } while (input > 0);
+        return sum;
     }
 }
