c8ee4d616be3a35bee4eb0533fd974fd60ba5eaa
Михаил Лоскутов <dev.toriki@gmail.com>
2023-07-16 22:51:36 +0500

    Добавлена задача на нахождение суммы элементов в массиве.

---
+++ src/LoafThePenguin.CSTraining/LoafThePenguin.CSTraining/Basic/ArrayDoubleItemsSum.cs
@@ -0,0 +1,14 @@
+using LoafThePenguin.CSTraining.Core;
+
+namespace LoafThePenguin.CSTraining.Basic;
+
+/*
+ * Найти сумму всех лементов массива.
+ */
+public sealed class ArrayDoubleItemsSum : IAlgorithm<double[], double>
+{
+    public double Run(double[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
