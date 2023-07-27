13618256b6f8200461c8f865b0fb5f2e09e03232
Михаил Лоскутов <dev.toriki@gmail.com>
2023-07-28 00:46:41 +0500

    Добавлена задача удаления пробелов в начале и конце строки.

---
+++ tests/LoafThePenguin.CSTraining.Tests/Basic/TrimWhitespacesTests.cs
@@ -0,0 +1,29 @@
+﻿using LoafThePenguin.CSTraining.Basic;
+using LoafThePenguin.CSTraining.Core.Tasks.Basic;
+
+namespace LoafThePenguin.CSTraining.Tests.Basic;
+
+public sealed class TrimWhitespacesTests
+{
+    private readonly TrimWhitespacesAlgorithm _algorithm;
+    private readonly TrimWhitespaces _trimWhitespaces;
+
+    public TrimWhitespacesTests()
+    {
+        _algorithm = new TrimWhitespacesAlgorithm();
+        _trimWhitespaces = new TrimWhitespaces();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("    ")]
+    [InlineData("  DJfsfdj ")]
+    [InlineData(" 9300234c")]
+    [InlineData(" abc ")]
+    [InlineData(" abc     ")]
+    [InlineData("sd fsd ff")]
+    public void TrimWhitespaces(string str)
+    {
+        Assert.Equal(_algorithm.Run(str), _trimWhitespaces.Run(str));
+    }
+}
