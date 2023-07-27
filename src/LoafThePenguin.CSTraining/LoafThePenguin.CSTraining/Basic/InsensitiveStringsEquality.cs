e7aa0b36f9acb3e09b1848a610acd0f19c349141
Михаил Лоскутов <dev.toriki@gmail.com>
2023-07-28 00:40:51 +0500

    Добавлена задача регистронезависимого сравнения строк.

---
+++ src/LoafThePenguin.CSTraining/LoafThePenguin.CSTraining/Basic/InsensitiveStringsEquality.cs
@@ -0,0 +1,15 @@
+using LoafThePenguin.CSTraining.Core;
+
+namespace LoafThePenguin.CSTraining.Basic;
+
+/*
+ * Реализовать регистронезависимое сравнение двух строк.
+ * Например: "AbC" будет равно "abc", но не равно "abcd"
+ */
+public sealed class InsensitiveStringsEquality : IAlgorithm<string, string, bool>
+{
+    public bool Run(string input1, string input2)
+    {
+        throw new NotImplementedException();
+    }
+}
