ffdbd258191df29342d28414db4769e10f57159e
Михаил Лоскутов <dev.toriki@gmail.com>
2023-07-28 13:49:26 +0500

    Добавлена задача "Найти букву в строке по порядковому номеру".

---
+++ src/LoafThePenguin.CSTraining/LoafThePenguin.CSTraining/Basic/GetCharBySequenceFromString.cs
@@ -0,0 +1,20 @@
+﻿using LoafThePenguin.CSTraining.Core;
+
+namespace LoafThePenguin.CSTraining.Basic;
+
+/*
+ * Найти букву в строке по порядковому номеру.
+ */
+public sealed class GetCharBySequenceFromString : IAlgorithm<string, int, char?>
+{
+    public char? Run(string str, int seqNum)
+    {
+        if (str.Length < seqNum)
+        {
+            return null;
+        }
+
+        // Писать код тут.
+        throw new NotImplementedException();
+    }
+}
