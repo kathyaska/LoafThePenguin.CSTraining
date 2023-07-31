96f0e3ed0d9887bbd71176d8cba74fd4e289de5b
Екатерина <kathyaska@mail.ru>
2023-07-31 21:44:33 +0300

    Самостоятельно решила задачу по пробелам в начале и в конце

--- src/LoafThePenguin.CSTraining/LoafThePenguin.CSTraining/Basic/TrimWhitespaces.cs
+++ src/LoafThePenguin.CSTraining/LoafThePenguin.CSTraining/Basic/TrimWhitespaces.cs
@@ -1,4 +1,4 @@
-﻿using LoafThePenguin.CSTraining.Core;
+using LoafThePenguin.CSTraining.Core;
 
 namespace LoafThePenguin.CSTraining.Basic;
 
@@ -6,6 +6,7 @@ public sealed class TrimWhitespaces : IAlgorithm<string, string>
 {
     public string Run(string input)
     {
-        throw new NotImplementedException();
+        string n = input.Trim();
+        return n;
     }
-}
+} //удалить пробелы в начале и в конце Trim— удаляет начальные и конечные пробелы из строки
